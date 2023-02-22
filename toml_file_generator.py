2f14d25a196bdfbc48e9569e22d39a1678c333d0
jlee <jlee@coinshares.com>
2023-02-22 11:21:37 +0000

    toml file generator ignores everything in eco dir

--- toml_file_generator.py
+++ toml_file_generator.py
@@ -5,9 +5,15 @@ with open("eco_list.json", "r") as f:
     list_of_ecos = json.loads(f.read())
 
 
+def return_list_of_ecos_pulled():
+    if os.path.exists("/home/ubuntu/repos/electric_capital_pull/eco"):
+        return [i for i in os.listdir("/home/ubuntu/repos/electric_capital_pull/eco")]
+    return []
+
+
 def toml_field_generator(path_of_toml_files):
     for ecosystems_by_numbers in os.listdir(path_of_toml_files):
 
-        for toml_file_parser in os.listdir(f"{path_of_toml_files}/{ecosystems_by_numbers}"):
+        for toml_file_parser in [i for i in os.listdir(f"{path_of_toml_files}/{ecosystems_by_numbers}") if i not in return_list_of_ecos_pulled()]:
             if toml_file_parser.replace(".toml", "") in list_of_ecos:
                 yield f"https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/{toml_file_parser[0]}/{toml_file_parser}"
