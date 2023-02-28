d9c820d36b910f2591f8525aaa5be5c9adef0813
jlee <jlee@coinshares.com>
2023-02-28 14:44:21 +0000

    explicity state agg func for pivot table

--- toml_file_generator.py
+++ toml_file_generator.py
@@ -15,5 +15,4 @@ def toml_field_generator(path_of_toml_files):
     for ecosystems_by_numbers in os.listdir(path_of_toml_files):
 
         for toml_file_parser in [i for i in os.listdir(f"{path_of_toml_files}/{ecosystems_by_numbers}") if i not in return_list_of_ecos_pulled()]:
-            if toml_file_parser.replace(".toml", "") in list_of_ecos:
-                yield f"https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/{toml_file_parser[0]}/{toml_file_parser}"
+            yield f"https://raw.githubusercontent.com/electric-capital/crypto-ecosystems/master/data/ecosystems/{toml_file_parser[0]}/{toml_file_parser}"
