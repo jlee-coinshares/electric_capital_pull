6f88664bbf56ff6fd23718d8d380ded38363a65d
jlee <jlee@coinshares.com>
2023-02-28 15:02:02 +0000

    explicity state agg func for pivot table

--- app.py
+++ app.py
@@ -15,10 +15,13 @@ if __name__ == '__main__':
 
     for url in tqdm(toml_field_generator(fr"{base_path}/crypto-ecosystems/data/ecosystems")):
         eco_file = download_eco_def(url)
-        generate_data(eco_file, limit=None)
-        combine_data(eco_file, 'commits')
-        combine_data(eco_file, 'authors')
-        post_process(eco_file)
+        try:
+            generate_data(eco_file, limit=None)
+            combine_data(eco_file, 'commits')
+            combine_data(eco_file, 'authors')
+            post_process(eco_file)
+        except KeyError:
+            continue
 
     aggregate_files(base_path)
     pivot_main()
